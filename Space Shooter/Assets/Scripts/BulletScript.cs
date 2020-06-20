16bb6920964a6200bb638500107d5921cd629fc1
Bobby <bobbyrobin8@gmail.com>
2020-06-20 13:16:19 +0530

    Implemented Shoot function

---
+++ Space Shooter/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private void Update()
+    {
+        Destroy(gameObject, 3f);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+
+
+    }
+}
